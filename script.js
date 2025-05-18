884867c2c48d80f7ab98d4797eba806173ee33a0
unknown <mjrajak47z@gmail.com>
2025-05-18 14:56:12 +0530

    all done

--- script.js
+++ script.js
@@ -22,6 +22,8 @@ fetch(' https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=mar
     console.error('Error:', error);
 });
 
+// fatch API
+
 async function fetchData() {
     try{
         const response = await fetch(' https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false');
